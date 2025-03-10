c06439b73af7b23d8ac49c9292fd38ed91bb6ced
Mason Simmons <116126049+MilkManIsHere@users.noreply.github.com>
2025-03-10 13:20:56 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -3,34 +3,42 @@ function searchResults() {
     var resultsContainer = document.getElementById("results");
     var links = {
         "1": "#",
-	"2": "#"
+        "2": "#"
     };
 
     resultsContainer.innerHTML = '';
+
     var matchingLinks = [];
 
     for (var term in links) {
-        if (links.hasOwnProperty(term) && (term.toLowerCase().indexOf(searchTerm) !== -1 || searchTerm === "")) {
-            matchingLinks.push({ term: term, link: links[term] });
+        if (links.hasOwnProperty(term)) {
+            if (term.toLowerCase().indexOf(searchTerm) !== -1 || searchTerm === "") {
+                matchingLinks.push({ term: term, link: links[term] });
+            }
         }
     }
 
     matchingLinks.sort(function(a, b) {
-        return a.term.localeCompare(b.term);
+        return a.term < b.term ? -1 : (a.term > b.term ? 1 : 0);
     });
 
     for (var i = 0; i < matchingLinks.length; i++) {
         var result = matchingLinks[i];
         var linkElement = document.createElement("a");
         linkElement.href = result.link;
-        linkElement.innerText = result.term;
 
         var fontElement = document.createElement("font");
+        fontElement.setAttribute("color", "blue");
         fontElement.setAttribute("face", "sans-serif");
         fontElement.setAttribute("size", "3");
-        fontElement.appendChild(linkElement);
 
-        resultsContainer.appendChild(fontElement);
+        var underlineElement = document.createElement("u");
+        underlineElement.appendChild(document.createTextNode(result.term));
+
+        fontElement.appendChild(underlineElement);
+        linkElement.appendChild(fontElement);
+
+        resultsContainer.appendChild(linkElement);
         resultsContainer.appendChild(document.createElement("br"));
     }
 }
